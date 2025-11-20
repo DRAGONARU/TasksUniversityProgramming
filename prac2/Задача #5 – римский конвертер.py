46f56c12d7dc721c689c0abd56fae7cd194d57fc
DRAGONARU <138095702+DRAGONARU@users.noreply.github.com>
2025-11-20 17:57:19 +0500

    Update Задача #5 – римский конвертер.py

--- prac2/Задача #5 – римский конвертер.py
+++ prac2/Задача #5 – римский конвертер.py
@@ -1,11 +1,21 @@
 import ast
 
 def extract_list_from_assignment(s):
+    '''
+    Функция, что парсит лист из входных данных.
+    Принимает на вход строку.
+    Возвращает список.
+    '''
     _, right = s.split('=', 1)
     result = ast.literal_eval(right.strip())
     return result
 
 def roman_to_int(roman):
+    '''
+    Функция для перевода римского числа в арабское.
+    Принимает на вход римское число.
+    Возвращает арабское.
+    '''
     roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
@@ -19,6 +29,11 @@ def roman_to_int(roman):
     return total
 
 def int_to_roman(num):
+    '''
+    Функция для перевода арабского числа в римское
+    Принимает на вход арабское число.
+    Возвращает римское.
+    '''
     num = int(num)
     roman_pairs = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),(100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
                    (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I') ]
@@ -30,6 +45,10 @@ def int_to_roman(num):
     return ''.join(result)
 
 def starting_box():
+    '''
+    Основная функция для конвертации чисел.
+    Возвращает массив в переведёнными числами.
+    '''
     numbers_list = extract_list_from_assignment(input())
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ans = []
@@ -41,4 +60,5 @@ def starting_box():
         for i in numbers_list:
             ans.append(int_to_roman(i))
         return ans
+
 print(starting_box())
